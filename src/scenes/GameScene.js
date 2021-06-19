11a801e31394963e6cfb1780dd9479887e692615
gaktahu358 <85770660+gaktahu358@users.noreply.github.com>
2021-06-19 10:37:21 +0700

    Initial commit

---
+++ src/scenes/GameScene.js
@@ -0,0 +1,28 @@
+import Phaser from 'phaser'
+import logo from '../../assets/Phaser-Logo-Small.png'
+
+class GameScene extends Phaser.Scene {
+  constructor () {
+    super({ key: 'GameScene' })
+  }
+
+  init () {}
+  preload () {
+    this.load.image('logo', logo)
+  }
+
+  create () {
+    const logo = this.add.image(400, 150, 'logo')
+
+    this.tweens.add({
+      targets: logo,
+      y: 450,
+      duration: 2000,
+      ease: 'Power2',
+      yoyo: true,
+      loop: -1
+    })
+  }
+}
+
+export default GameScene
